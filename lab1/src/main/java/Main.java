33eb196c8ea91995f31ec551c9a8f19a61592bbc
Anastasia Kravchenko <anastasia.kravchenko@sibers.com>
2023-02-15 18:14:49 +0700

    set up logger

--- lab1/src/main/java/Main.java
+++ lab1/src/main/java/Main.java
@@ -1,5 +1,9 @@
+
 public class Main {
+
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.debug("Hello world!");
     }
 }
