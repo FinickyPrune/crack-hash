157fe30d18a877d1d39748bbb9cbea75972fe5f7
FinickyPrune <a.kravchenko2@g.nsu.ru>
2023-03-28 17:47:43 +0700

    inited modules

---
+++ crackhash/centralmanager/src/main/java/ru/nsu/kravchenko/crackhash/centralmanager/CentralmanagerApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.kravchenko.crackhash.centralmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralmanagerApplication.class, args);
+	}
+
+}
---
+++ crackhash/centralmanager/src/test/java/ru/nsu/kravchenko/crackhash/centralmanager/CentralmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.kravchenko.crackhash.centralmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
