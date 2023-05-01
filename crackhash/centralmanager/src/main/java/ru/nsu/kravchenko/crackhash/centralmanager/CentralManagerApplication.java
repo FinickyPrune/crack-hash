487b52e5ddafb67abfc8c3dff6a2443930dfa98f
FinickyPrune <a.kravchenko2@g.nsu.ru>
2023-05-01 18:55:56 +0700

    added mongo

--- crackhash/centralmanager/src/main/java/ru/nsu/kravchenko/crackhash/centralmanager/CentralManagerApplication.java
+++ crackhash/centralmanager/src/main/java/ru/nsu/kravchenko/crackhash/centralmanager/CentralManagerApplication.java
@@ -2,6 +2,8 @@ package ru.nsu.kravchenko.crackhash.centralmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
